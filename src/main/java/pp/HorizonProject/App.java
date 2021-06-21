be59a1a1bd6b8eae45ec06c67e6c24f385e496cc
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2021-06-21 16:03:32 +0200

    updated App and Menu, user input complete

--- src/main/java/pp/HorizonProject/App.java
+++ src/main/java/pp/HorizonProject/App.java
@@ -3,49 +3,66 @@ package pp.HorizonProject;
 import java.io.IOException;
 import java.lang.invoke.SwitchPoint;
 
-
 public class App {
-	public static void main(String[] args) throws IOException {
-		System.out.println("Aktywna ścieżka: " + args[0]);
-		
-		Importer importer = new Importer();
-		importer.readFieles();//args[0]);
-		
-		
 	
+	public static void main(String[] args) throws IOException {
+		int userInput;
+		int reportYear;
+		String workerName;
+		String projectName;
+
+		System.out.println("Aktywna ścieżka: " + args[0]);
+		Importer importer = new Importer();
+		// importer.readFieles();//args[0]);
+
 		while (true) {
 			Menu.printMenu();
-			int userInput = Menu.getReportFromUser();
+			userInput = Menu.getReportFromUser();
 			switch (userInput) {
 			case 1:
-				System.out.println("Raport1");
-				Menu.getYearFromUser();
+				reportYear = Menu.getYearFromUser();
+				System.out.println("Raport 1");
 				Report1 report = new Report1(importer.dataModel);
-				report.printReport(2012);
+				report.printReport(reportYear);
 				break;
 			case 2:
-				System.out.println("Raport2");
+				reportYear = Menu.getYearFromUser();
+				projectName = Menu.getProjectFromUser();
+				System.out.println("Raport 2");
+
 				break;
 			case 3:
-				System.out.println("Raport3");
+				reportYear = Menu.getYearFromUser();
+				workerName = Menu.getWorkerFromUser();
+				System.out.println("Raport 3");
+
 				break;
 			case 4:
-				System.out.println("Raport4");
+				reportYear = Menu.getYearFromUser();
+				System.out.println("Raport 4");
+
 				break;
 			case 5:
-				System.out.println("Raport5");
+				projectName = Menu.getProjectFromUser();
+				System.out.println("Raport 5");
+
 				break;
 			case 6:
-				System.out.println("Raport6");
+				reportYear = Menu.getYearFromUser();
+				System.out.println("Raport 6 - Wykres słupkowy");
+
 				break;
 			case 7:
-				System.out.println("Raport7");
+				reportYear = Menu.getYearFromUser();
+				workerName = Menu.getWorkerFromUser();
+				System.out.println("Raport 7 - Wykres kołowy");
+
 				break;
 			case 0:
-				System.out.println("Zakonczono");
+				System.out.println("Zakończono...");
 				System.exit(0);
 			default:
-				System.out.println("Niepoprawne dane");
+				System.out.println("Wybierz opcję od 0 do 7");
 			}
 		}
 	}
